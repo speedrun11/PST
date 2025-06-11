ee3d974b307838d49986dcf4cdb487effc0938c3
speedrun11 <jefferssond.03@gmail.com>
2025-06-12 01:25:43 +0800

    ADD DATABASE FILE

---
+++ DATABASE FILE/rposystem.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2025 at 07:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rposystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_admin`
+--
+
+CREATE TABLE `rpos_admin` (
+  `admin_id` varchar(200) NOT NULL,
+  `admin_name` varchar(200) NOT NULL,
+  `admin_email` varchar(200) NOT NULL,
+  `admin_password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rpos_admin`
+--
+
+INSERT INTO `rpos_admin` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
+('9ad012jksad01sakxncasu12e3sad', 'System Admin', 'pastilsatabi.pagasa@gmail.com', '$2y$10$.39zVClTEfQddJb2Ize54eIOQlFT08hWs5rIvSANvYUOAZJhu.Ut2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_customers`
+--
+
+CREATE TABLE `rpos_customers` (
+  `customer_id` varchar(200) NOT NULL,
+  `customer_name` varchar(200) NOT NULL,
+  `customer_phoneno` varchar(200) NOT NULL,
+  `customer_email` varchar(200) NOT NULL,
+  `customer_password` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `verification_token` varchar(64) DEFAULT NULL,
+  `is_verified` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rpos_customers`
+--
+
+INSERT INTO `rpos_customers` (`customer_id`, `customer_name`, `customer_phoneno`, `customer_email`, `customer_password`, `created_at`, `verification_token`, `is_verified`) VALUES
+('CUS6849a6c26354d6082', 'Jeffersson Divina', '09754315633', 'jefferssond.03@gmail.com', 'adcd7048512e64b48da55b027577886ee5a36350', '2025-06-11 15:55:11.556916', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_orders`
+--
+
+CREATE TABLE `rpos_orders` (
+  `order_id` varchar(200) NOT NULL,
+  `order_code` varchar(200) NOT NULL,
+  `customer_id` varchar(200) NOT NULL,
+  `customer_name` varchar(200) NOT NULL,
+  `prod_id` varchar(200) NOT NULL,
+  `prod_name` varchar(200) NOT NULL,
+  `prod_price` varchar(200) NOT NULL,
+  `prod_qty` varchar(200) NOT NULL,
+  `order_status` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_pass_resets`
+--
+
+CREATE TABLE `rpos_pass_resets` (
+  `reset_id` int(20) NOT NULL,
+  `reset_code` varchar(200) NOT NULL,
+  `reset_token` varchar(200) NOT NULL,
+  `reset_email` varchar(200) NOT NULL,
+  `reset_status` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_payments`
+--
+
+CREATE TABLE `rpos_payments` (
+  `pay_id` varchar(200) NOT NULL,
+  `pay_code` varchar(200) NOT NULL,
+  `order_code` varchar(200) NOT NULL,
+  `customer_id` varchar(200) NOT NULL,
+  `pay_amt` varchar(200) NOT NULL,
+  `pay_method` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_products`
+--
+
+CREATE TABLE `rpos_products` (
+  `prod_id` varchar(200) NOT NULL,
+  `prod_code` varchar(200) NOT NULL,
+  `prod_name` varchar(200) NOT NULL,
+  `prod_img` varchar(200) NOT NULL,
+  `prod_desc` longtext NOT NULL,
+  `prod_price` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rpos_staff`
+--
+
+CREATE TABLE `rpos_staff` (
+  `staff_id` int(20) NOT NULL,
+  `staff_name` varchar(200) NOT NULL,
+  `staff_number` varchar(200) NOT NULL,
+  `staff_email` varchar(200) NOT NULL,
+  `staff_password` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rpos_admin`
+--
+ALTER TABLE `rpos_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `rpos_customers`
+--
+ALTER TABLE `rpos_customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `rpos_orders`
+--
+ALTER TABLE `rpos_orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `CustomerOrder` (`customer_id`),
+  ADD KEY `ProductOrder` (`prod_id`);
+
+--
+-- Indexes for table `rpos_pass_resets`
+--
+ALTER TABLE `rpos_pass_resets`
+  ADD PRIMARY KEY (`reset_id`);
+
+--
+-- Indexes for table `rpos_payments`
+--
+ALTER TABLE `rpos_payments`
+  ADD PRIMARY KEY (`pay_id`),
+  ADD KEY `order` (`order_code`);
+
+--
+-- Indexes for table `rpos_products`
+--
+ALTER TABLE `rpos_products`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indexes for table `rpos_staff`
+--
+ALTER TABLE `rpos_staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rpos_pass_resets`
+--
+ALTER TABLE `rpos_pass_resets`
+  MODIFY `reset_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `rpos_staff`
+--
+ALTER TABLE `rpos_staff`
+  MODIFY `staff_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rpos_orders`
+--
+ALTER TABLE `rpos_orders`
+  ADD CONSTRAINT `CustomerOrder` FOREIGN KEY (`customer_id`) REFERENCES `rpos_customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ProductOrder` FOREIGN KEY (`prod_id`) REFERENCES `rpos_products` (`prod_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
